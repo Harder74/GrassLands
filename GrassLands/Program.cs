241f442087d12b1142d66843daa08ffe97e1ff08
Harder74 <tylerdrewharder@ksu.edu>
2022-01-27 19:05:36 -0600

    Add project files.

---
+++ GrassLands/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GrassLands
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new GrassLands())
+                game.Run();
+        }
+    }
+}
